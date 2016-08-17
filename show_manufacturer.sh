6e849e3bd3028443c1fb90624b75870f9e27fc52
André Déo <andredeo@gmail.com>
2016-08-17 16:08:54 -0300

    Update show_manufacturer.sh

--- show_manufacturer.sh
+++ show_manufacturer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##########################
+# DEFINICAO DE VARIAVEIS #
+##########################
+
+# Coloque aqui o caminho onde o script sera armazenado
+# O arquivo com as definicoes da IANA sera armazenado no mesmo local
+
+scrip_path=/Scripts/Scripts_Zabbix
+
 ########################
 # DEFINICAO DE FUNCOES #
 ########################
@@ -10,20 +19,31 @@
 
 iana ()
 {
-if [ ! -s iana.txt ]
+if [ ! -s $scrip_path/iana.txt ]
 then
         echo "Baixando lista de OIDs Enterprise da IANA, aguarde por favor ..."
-        curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers > iana.txt
+        curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers > $scrip_path/iana.txt
         echo " "
         echo "Processo finalizado"
 else
-        if [ `grep 'last updated' iana.txt | cut -d" " -f3 | cut -d')' -f1` != `curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers | head -3 | tail -1 | cut -d" " -f3 | cut -d')' -f1` ]
+        if [ `grep 'last updated' $scrip_path/iana.txt | cut -d" " -f3 | cut -d')' -f1` != `curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers | head -3 | tail -1 | cut -d" " -f3 | cut -d')' -f1` ]
         then
                 echo "Atualizando lista de OIDs Enterprise da IANA, aguarde por favor ..."
-                curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers > iana.txt
+                curl -s http://www.iana.org/assignments/enterprise-numbers/enterprise-numbers > $scrip_path/iana.txt
                 echo " "
                 echo "Processo finalizado"
         fi
+fi
+}
+
+# Funcao "oid"
+# Recorta do OID a parte que contem o fabricante do equipamento
+# Pesquisa o Fabricante no arquivo e retorna o valor encontrado
+
+oid ()
+{
+oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
+sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
 fi
 }
 
@@ -36,40 +56,36 @@ snmp_01)iana
         snmp=$(snmpget -Oqn -v1 -c "$2" "$3" sysObjectID.0)
         if [ $? -eq 0 ]
         then
-        oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
-        sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
+        oid
         fi
         ;;
+
 snmp_02)iana
         snmp=$(snmpget -Oqn -v2c -c "$2" "$3" sysObjectID.0)
         if [ $? -eq 0 ]
         then
-        oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
-        sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
+        oid
         fi
         ;;
 snmp_03)iana
         snmp=$(snmpget -Oqn -v3 -u "$2" -n "" -l noAuthNoPriv "$3" sysObjectID.0)
         if [ $? -eq 0 ]
         then
-        oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
-        sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
+        oid
         fi
         ;;
 snmp_04)iana
         snmp=$(snmpget -Oqn -v3 -u "$2" -n "" -l authNoPriv -a "$4" -A "$5" "$3" sysObjectID.0)
         if [ $? -eq 0 ]
         then
-        oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
-        sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
+        oid
         fi
         ;;
 snmp_05)iana
         snmp=$(snmpget -Oqn -v3 -u "$2" -n "" -x "$6" -X "$7" -l authPriv -a "$4" -A "$5" "$3" sysObjectID.0)
         if [ $? -eq 0 ]
         then
-        oid=$(echo $snmp | cut -d ' ' -f2 | cut -d'.' -f8)
-        sed -n "/^$oid$/{n;p;}" iana.txt | tr -s ' ' | cut -d' ' -f2
+        oid
         fi
         ;;
 *) echo "Uso: ./$0 snmp_01 \"comunidade\" \"IP\" - Para SNMP v1
